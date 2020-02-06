2ba3a015b895873644fae4887e5f13643a143e48
Jimmy Maple <jmaple@splunk.com>
2020-02-05 21:46:38 -0500

    Initial commit

---
+++ bin/addMetricsInfo.ps1
@@ -0,0 +1,81 @@
+## Customer Variable - fill in with customer value
+$CUSTOMER = "customer"
+
+## Variables used in the script
+$APPHOME = $SplunkHome + "\etc\apps\"+ $CUSTOMER +"_metrics_meta_settings"
+$INPUTCONF = $APPHOME + "\local\inputs.conf"
+$METRICSPARAM = "logical_disk,physical_disk,cpu,memory,network,system,process"
+$CHECKPOINT = $SplunkHome + "\etc\metricsCheckpoint"
+$METAPATH = $SPLUNKHOME + "\etc\restart_meta.txt"
+
+## Check for checkpoint value and exit if it exists
+if (Test-Path "$CHECKPOINT") {
+exit
+}
+
+## Create the app directoy if it doesn't exist
+if (-not (Test-Path "$APPHOME")) {
+	New-Item "$APPHOME\local" -ItemType Directory | Out-Null
+	New-Item "$APPHOME\metadata" -ItemType Directory | Out-Null
+	echo '[]
+access = read : [ * ], write : [ admin ]
+export = system' > "$APPHOME\metadata\local.meta"
+}
+
+## Determine if the host is in AWS and capture AWS related dimensions
+if (Test-Path "C:\Windows\Amazon\Ec2ConfigService") {
+  $instance_id = Invoke-RestMethod -Uri http://169.254.169.254/latest/meta-data/instance-id
+  $account_id = (Invoke-RestMethod -Uri http://169.254.169.254/latest/meta-data/identity-credentials/ec2/info/).accountId
+  $region = Invoke-RestMethod -Uri http://169.254.169.254/latest/meta-data/placement/availability-zone
+  $cloud_dims = " region::$region instance_id::$instance_id account_id::$account_id"
+}
+
+## List of inputs that will be added to the inputs.conf
+$m_cpu ="[perfmon://CPU]`r`n" `
++ "disabled = 0"
+
+$m_memory ="[perfmon://Memory]`r`n" `
++ "disabled = 0"
+
+$m_physical_disk ="[perfmon://PhysicalDisk]`r`n" `
++ "disabled = 0"
+
+$m_logical_disk ="[perfmon://LogicalDisk]`r`n" `
++ "disabled = 0"
+
+$m_network ="[perfmon://Network]`r`n" `
++ "disabled = 0"
+
+$m_system ="[perfmon://System]`r`n" `
++ "disabled = 0"
+
+$m_process ="[perfmon://Process]`r`n" `
++ "disabled = 0"
+
+## Capture the OS and IP details of the host
+$os_info = Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version
+$ip_info = Test-Connection -ComputerName $env:computername -count 1 | Select-Object IPV4Address
+
+## Construct the dimensions into a single string together
+$dims = "os::" + "`"" + $os_info.Caption + "`""
+$dims = $dims + " os_version::" + $os_info.Version
+$dims = $dims + " ip::" + "`"" + $ip_info.IPV4Address.IPAddressToString + "`""
+$dims = $dims + ' entity_type::Windows_Host'
+if (-not ([string]::IsNullOrEmpty($cloud_dims))) {
+$dims = $dims + $cloud_dims
+}
+
+## Create the inputs.conf in the app and populate it with the stanzas and dimensions
+$metrics = $METRICSPARAM -split ','
+echo '' > $INPUTCONF
+For ($i=0; $i -lt $metrics.Length; $i++) {
+  $m_name = "m_" + $metrics[$i]
+  Get-Variable -Name $m_name -ValueOnly -ErrorAction 'Ignore' >> $INPUTCONF
+  # Add dimensions
+  echo "_meta = $dims" >> $INPUTCONF
+  echo "`n" >> $INPUTCONF
+}
+
+## Set up checkpoint value to ensure the script only runs once and create restart file
+echo '' > $CHECKPOINT
+echo '' > $METAPATH
---
+++ bin/restart.ps1
@@ -0,0 +1,17 @@
+### Configure path to restart_check.txt
+$metaPath = "$SPLUNKHOME\etc\restart_meta.txt"
+
+### Filter to attach timestamps where necessary
+filter timestamp {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss.fff') ${env:COMPUTERNAME}: $_"}
+
+if ($metaPath -eq "True") {
+	Write-output "Meta settings has been changed." | timestamp
+	Write-output "Restarting forwarder." | timestamp
+	if ($metaPath -eq "True") {
+		Remove-Item -path "$metaPath"
+	}
+	Remove-Item -path "$SPLUNKHOME\etc\apps\metrics_meta_app\DeleteMeToRestart"
+} else {
+	Write-output "No settings have been changed." | timestamp
+	Write-output "No restart required." | timestamp
+}
