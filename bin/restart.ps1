a27412150743a62549ef47c89ea614a68a0ddcb3
Jimmy Maple <jmaple@splunk.com>
2020-07-29 14:50:43 -0400

    Update restart.ps1
    Updated restart script properly check for a restart file then remove the "DeleteMeToRestart" file

--- bin/restart.ps1
+++ bin/restart.ps1
@@ -1,13 +1,14 @@
 ### Configure path to restart_check.txt
 $metaPath = "$SPLUNKHOME\etc\restart_meta.txt"
+$restartMeta = $(Test-Path "$SPLUNKHOME\etc\restart_meta.txt" -PathType Leaf)
 
 ### Filter to attach timestamps where necessary
 filter timestamp {"$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss.fff') ${env:COMPUTERNAME}: $_"}
 
-if ($metaPath -eq "True") {
+if ($restartMeta -eq "True") {
 	Write-output "Meta settings has been changed." | timestamp
 	Write-output "Restarting forwarder." | timestamp
-	if ($metaPath -eq "True") {
+	if ($restartMeta -eq "True") {
 		Remove-Item -path "$metaPath"
 	}
 	Remove-Item -path "$SPLUNKHOME\etc\apps\MetricsMetaConfigurationApp\DeleteMeToRestart"
